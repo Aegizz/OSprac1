9b2828997df71f6d8fde4a24e3a5a6fa3c009c2e
lloyddraysey@gmail.com <lloyddraysey@gmail.com>
2024-08-03 11:09:34 +0000

    zero

--- even.c
+++ even.c
@@ -9,23 +9,19 @@ void handle_sighandle(int sig);
 int main(int argc, char *argv[]){
     signal(SIGINT, &handle_sighandle);
     signal(SIGHUP, &handle_sighandle);
-    // int n = atoi(argv[1]);
-    // for (int i = 0; i <= n; i = i + 2){
-    //     printf("%d\n", i);
-    //     fflush(stdout);
-    //     sleep(5);
-    // }
+    int n = atoi(argv[1]);
+    for (int i = 0; i <= n; i = i + 2){
+        printf("%d\n", i);
+    }
     return 0;
 }
 
 void handle_sighandle(int sig){
     if (sig == SIGHUP){
         printf("Ouch!\n");
-        fflush(stdout);
 
     } else if (sig == SIGINT){
         printf("Yeah!\n");
-        fflush(stdout);
 
     }
 }
